555f8edc08d273d86e463248b103c72b4481b0dd
neilkidd <Neil.Kidd@7digital.com>
2014-08-08 12:11:45 +0100

    Initial commit

---
+++ AlienInvasion/EarthDefender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlienInvasion.Client;
+using AlienInvasion.Client.AlienInvaders;
+using AlienInvasion.Client.DefenceAssets;
+
+namespace AlienInvasion
+{
+	public class EarthDefender : IEarthDefender
+	{
+
+		public DefenceStrategy DefendEarth(IAlienInvasionWave invasionWave)
+		{
+		    return new DefenceStrategy(invasionWave.WeaponsAvailableForDefence.Take(invasionWave.AlienInvaders.Count()));
+		}
+
+
+	}
+}
